5dcfb497d88b54dcdc2ce5c3b77a9f055ebe8a61
Instantlysi <boris.the.alien@gmail.com>
2020-06-04 12:13:57 +0100

    Showing on air functionality, auto update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
 namespace SWLogger
 {
     /// <summary>
@@ -21,18 +21,50 @@ namespace SWLogger
     /// </summary>
     public partial class MainWindow : Window
     {
-        SettingsWindow settingsWindow = new SettingsWindow();
-        string dataPath;
-        string historyPath;
+        SettingsWindow settingsWindow = new SettingsWindow();        
         Schedule scheduleE;
         List<Schedule> schEntries = new List<Schedule>();
+        List<Schedule> onAirEntries = new List<Schedule>();
+        DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        DispatcherTimer minuteTick = new System.Windows.Threading.DispatcherTimer();
+        string dataPath;
+        string historyPath;
+        string[] schedEntry;
+        DateTime UTC = DateTime.UtcNow;
         public MainWindow()
         {
             InitializeComponent();
+
             
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(200);
+            dispatcherTimer.Start();
+
+            minuteTick.Tick += new EventHandler(minuteTick_Tick);
+            minuteTick.Interval = new TimeSpan(0, 1, 0);
+            minuteTick.Start();
+
             dataPath = settingsWindow.dataPath;
             historyPath = settingsWindow.historyPath;
+            schedEntry = File.ReadAllLines(dataPath);
             DrawLive();
+        }
+
+        private void minuteTick_Tick(object sender, EventArgs e)
+        {
+            DrawLive();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UTC = DateTime.UtcNow;
+            ClockUTC.Content = UTC.TimeOfDay.ToString();
+        }
+
+        private void OnAirGrid_MouseClick(object sender, MouseEventArgs e)
+        {
+            
         }
 
         protected override void OnClosed(EventArgs e)
@@ -44,14 +76,19 @@ namespace SWLogger
 
         private void DrawLive()
         {
-            string[] schedEntry = File.ReadAllLines(dataPath);
             
+            TimeSpan now = UTC.TimeOfDay;
             foreach (string entry in schedEntry)
-            {
+            {                
                 string[] split = entry.Split(';');
                 scheduleE = new Schedule(split);
                 schEntries.Add(scheduleE);
-                OnAirGrid.Items.Add(scheduleE);
+                if((now > scheduleE.StartTime) && (now < scheduleE.EndTime))
+                {
+                    onAirEntries.Add(scheduleE);
+                    OnAirGrid.Items.Add(scheduleE);
+                }
+                
             }
             
         }
@@ -65,5 +102,31 @@ namespace SWLogger
         {
             settingsWindow.ShowDialog();
         }
+
+        private void OnAirGrid_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ContextMenu m = new ContextMenu();
+            m.Items.Add("Quick Add");
+            m.Items.Add("Add with notes");
+            m.IsOpen = true;
+
+            HitTestResult hitTestResult = VisualTreeHelper.HitTest(OnAirGrid, e.GetPosition(OnAirGrid));
+            DataGridRow dataGridRow = hitTestResult.VisualHit.GetParentOfType<DataGridRow>();
+            int index = dataGridRow.GetIndex();
+        }
+    }
+    public static class Extensions
+    {
+        public static T GetParentOfType<T>(this DependencyObject element) where T : DependencyObject
+        {
+            Type type = typeof(T);
+            if (element == null) return null;
+            DependencyObject parent = VisualTreeHelper.GetParent(element);
+            if (parent == null && ((FrameworkElement)element).Parent is DependencyObject) parent = ((FrameworkElement)element).Parent;
+            if (parent == null) return null;
+            else if (parent.GetType() == type || parent.GetType().IsSubclassOf(type)) return parent as T;
+            return GetParentOfType<T>(parent);
+        }
     }
 }
+
--- Schedule.cs
+++ Schedule.cs
@@ -10,26 +10,40 @@ namespace SWLogger
     class Schedule
     {
         public string Frequency { get; }
+        public TimeSpan StartTime { get; }
+        public TimeSpan EndTime { get; }
         public string BroadcastTime { get; }
         public string Station { get; }
         public string Country { get; }
         public string Language { get; }
         public string Days { get; }
-               
+              
         public Schedule()
         {
             Frequency = "";
-            BroadcastTime = "";
+            StartTime = new TimeSpan(0,0,0);
+            EndTime = new TimeSpan(0, 0, 0);
             Station  = "";
             Country = "";
             Language = "";
             Days = "";
+            BroadcastTime = "";
         }
 
         public Schedule(string[] entry)
         {
-            Frequency = entry[0];
+
+            string[] timing = entry[1].Split('-');
+            timing[0] = timing[0].Insert(2, ":");
+            timing[1] = timing[1].Insert(2, ":");
+            if (timing[1] == "24:00")
+            {
+                timing[1] = "23:59";
+            }
+            StartTime = TimeSpan.Parse(timing[0]);
+            EndTime = TimeSpan.Parse(timing[1]);
             BroadcastTime = entry[1];
+            Frequency = entry[0];
             Station = entry[4];
             Country = entry[3];
             Language = entry[5];
